637ac891ee4a0ec0b268ad400b2fdaf90269a263
Furqan Khanzada <furqan.khanzada@gmail.com>
2016-08-19 19:49:05 +0500

    register initialized

--- imports/ui/components/AuthLayout.jsx
+++ imports/ui/components/AuthLayout.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import { AppBar } from 'react-toolbox';
-import { Layout, Panel, Input } from 'react-toolbox';
+import { Layout, Panel, Input, Card, Button } from 'react-toolbox';
 
 // App component - represents the whole app
 export default class AuthLayout extends Component {
@@ -17,16 +17,21 @@ export default class AuthLayout extends Component {
                     <AppBar>
                         FinanceBakerZ
                     </AppBar>
-                    <div style={{ flex: 1, display: 'flex' }}>
-                        <Input type='text' label='Name'
-                               name='name'
-                               maxLength={ 25 }
-                               required
-                            />
-                        <Input type='text' label='Purpose'
-                               name='purpose'
-                               maxLength={ 50 }
-                            />
+                    <div style={{ flex: 1, display: 'flex', alignItems: 'center', justifyContent: 'center', overflowY: 'auto'}}>
+                        <Card style={{width: '350px', padding: '1.8rem', margin: '25px 0 25px 0'}}>
+                            <form>
+                                <Input type='text' label='Username or Email'
+                                       name='username-or-email'
+                                       maxLength={ 30 }
+                                       required
+                                    />
+                                <Input type='password' label='Password'
+                                       name='password'
+                                       maxLength={ 20 }
+                                    />
+                                <Button icon='person_add' label='Register' raised primary />
+                            </form>
+                        </Card>
                     </div>
                 </Panel>
             </Layout>
