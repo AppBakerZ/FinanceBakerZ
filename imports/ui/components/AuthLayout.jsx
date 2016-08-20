b60a245cb0b4ed9e7a26b68cfec0a2d65bc1f2aa
Furqan Khanzada <furqan.khanzada@gmail.com>
2016-08-20 17:50:16 +0500

    add link to main title

--- imports/ui/components/AuthLayout.jsx
+++ imports/ui/components/AuthLayout.jsx
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import { AppBar } from 'react-toolbox';
 import { Layout, Panel, Input, Card, Button } from 'react-toolbox';
 
+import { Link } from 'react-router'
+
 // App component - represents the whole app
 export default class AuthLayout extends Component {
 
@@ -15,7 +17,10 @@ export default class AuthLayout extends Component {
             <Layout>
                 <Panel>
                     <AppBar>
-                        FinanceBakerZ
+                        <Link
+                            to={`/`}>
+                            FinanceBakerZ
+                        </Link>
                     </AppBar>
                     <div style={{ flex: 1, display: 'flex', alignItems: 'center', justifyContent: 'center', overflowY: 'auto'}}>
                         <Card style={{width: '350px', padding: '1.8rem', margin: '25px 0 25px 0'}}>
