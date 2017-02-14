362987bc754d6aad0f30a02d5b019d06496fd0a6
zubair <zubairkhanzada7@gmail.com>
2017-02-14 19:06:05 +0500

    bank icons updates

--- imports/ui/bankFonts.js
+++ imports/ui/bankFonts.js
@@ -102,5 +102,229 @@ export default {
             label: 'Payment Skrill',
             value: 'bank-Payment-Skrill'
         }
+    ],
+    AE: [
+        {
+            label: 'Abu Dhabi Commercial Bank',
+            value: 'bank-Abu-Dhabi-Commercial-Bank'
+        },
+        {
+            label: 'Abu Dhabi Islamic Bank',
+            value: 'bank-Abu-Dhabi-Islamic-Bank'
+        },
+        {
+            label: 'Arab Bank',
+            value: 'bank-Arab-Bank'
+        },
+        {
+            label: 'Bank of Sharjah',
+            value: 'bank-Bank-of-Sharjah'
+        },
+        {
+            label: 'Citi Bank',
+            value: 'bank-Citi-Bank'
+        },
+        {
+            label: 'Commercial Bank of Dubai',
+            value: 'bank-Commercial-Bank-of-Dubai'
+        },
+        {
+            label: 'Emirates NBD',
+            value: 'bank-Emirates-NBD'
+        },
+        {
+            label: 'First Gulf Bank',
+            value: 'bank-First-Gulf-Bank'
+        },
+        {
+            label: 'HSBC Bank',
+            value: 'bank-HSBC-Bank'
+        },
+        {
+            label: 'Mashreq Bank',
+            value: 'bank-Mashreq-Bank'
+        },
+        {
+            label: 'National Bank of Abu Dubai',
+            value: 'bank-National-Bank-of-Abu-Dubai'
+        },
+        {
+            label: 'Noor Bank',
+            value: 'bank-Noor-Bank'
+        },
+        {
+            label: 'Rak Bank',
+            value: 'bank-Rak-Bank'
+        },
+        {
+            label: 'Sharjah Islamic Bank',
+            value: 'bank-Sharjah-Islamic-Bank'
+        },
+        {
+            label: 'Union National Bank',
+            value: 'bank-Union-National-Bank'
+        },
+        {
+            label: 'Emirates Investment Bank',
+            value: 'bank-Emirates-Investment-Bank'
+        }
+    ],
+    QA: [
+        {
+            label: 'Ahli Bank',
+            value: 'bank-Ahli-Bank'
+        },
+        {
+            label: 'Doha Bank',
+            value: 'bank-Doha-Bank'
+        },
+        {
+            label: 'International Bank of Qatar',
+            value: 'bank-International-Bank-of-Qatar'
+        },
+        {
+            label: 'Qatar Development Bank',
+            value: 'bank-Qatar-Development-Bank'
+        },
+        {
+            label: 'Qatar Islamic Bank',
+            value: 'bank-Qatar-Islamic-Bank'
+        },
+        {
+            label: 'Qatar National Bank',
+            value: 'bank-Qatar-National-Bank'
+        }
+    ],
+    SA: [
+        {
+            label: 'Alinma Bank',
+            value: 'bank-Alinma-Bank'
+        },
+        {
+            label: 'Al Rajhi Bank',
+            value: 'bank-Al-Rajhi-Bank'
+        },
+        {
+            label: 'Arab National Bank',
+            value: 'bank-Arab-National-Bank'
+        },
+        {
+            label: 'Bank Bilad',
+            value: 'bank-Bank-Bilad'
+        },
+        {
+            label: 'Riyad Bank',
+            value: 'bank-Riyad-Bank'
+        },
+        {
+            label: 'Saudi Hollandi Bank',
+            value: 'bank-Saudi-Hollandi-Bank'
+        }
+    ],
+    TR: [
+        {
+            label: 'Adabank',
+            value: 'bank-Adabank'
+        },
+        {
+            label: 'Akbank',
+            value: 'bank-Akbank'
+        },
+        {
+            label: 'Anadolu Bank',
+            value: 'bank-Anadolu-Bank'
+        },
+        {
+            label: 'Bank Mellat',
+            value: 'bank-Bank-Mellat'
+        },
+        {
+            label: 'Birlesik Fon Bankasi',
+            value: 'bank-Birlesik-Fon-Bankasi'
+        },
+        {
+            label: 'Credit Agricole',
+            value: 'bank-Credit-Agricole'
+        },
+        {
+            label: 'Deniz Bank',
+            value: 'bank-Deniz-Bank'
+        },
+        {
+            label: 'Deutsche Bank',
+            value: 'bank-Deutsche-Bank'
+        },
+        {
+            label: 'Fibab Bank',
+            value: 'bank-Fibab-Bank'
+        },
+        {
+            label: 'Finans Bank',
+            value: 'bank-Finans-Bank'
+        },
+        {
+            label: 'Halk Bank',
+            value: 'bank-Halk-Bank'
+        },
+        {
+            label: 'ICBC Turkey',
+            value: 'bank-ICBC-Turkey'
+        },
+        {
+            label: 'ING Group',
+            value: 'bank-ING-Group'
+        },
+        {
+            label: 'Intesa Sanpaolo',
+            value: 'bank-Intesa-Sanpaolo'
+        },
+        {
+            label: 'Merrill Lynch',
+            value: 'bank-Merrill-Lynch'
+        },
+        {
+            label: 'Rabo Bank',
+            value: 'bank-Rabo-Bank'
+        },
+        {
+            label: 'Royal Bank of Scotland',
+            value: 'bank-Royal-Bank-of-Scotland'
+        },
+        {
+            label: 'Societe Generale',
+            value: 'bank-Societe-Generale'
+        },
+        {
+            label: 'Turkish Bank',
+            value: 'bank-Turkish-Bank'
+        },
+        {
+            label: 'Turkish Economic Bank',
+            value: 'bank-Turkish-Economic-Bank'
+        },
+        {
+            label: 'Turkiye is Bankasi',
+            value: 'bank-Turkiye-is-Bankasi'
+        },
+        {
+            label: 'Vakif Bank',
+            value: 'bank-Vakif-Bank'
+        },
+        {
+            label: 'Yapi ve Kredi Bankasi',
+            value: 'bank-Yapi-ve-Kredi-Bankasi'
+        },
+        {
+            label: 'Ziraat Bank',
+            value: 'bank-Ziraat-Bank'
+        },
+        {
+            label: 'Sekerbank',
+            value: 'bank-Sekerbank'
+        },
+        {
+            label: 'AnT Bank',
+            value: 'bank-AnT-Bank'
+        }
     ]
 }
