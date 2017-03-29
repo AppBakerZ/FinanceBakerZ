87bd06c5292d3f5b3dfb12f4559b5f07a4335cf9
furqan khanzada <furqan.khanzada@gmail.com>
2017-03-29 13:39:35 +0500

    fixed z-index issue and enhanced style #63

--- imports/ui/components/connectionStatus/ConnectionStatus.jsx
+++ imports/ui/components/connectionStatus/ConnectionStatus.jsx
@@ -7,7 +7,6 @@ import theme from './theme';
 
 
 export default class ConnectionStatus extends Component {
-
     constructor(props) {
         super(props);
         this.state = {
@@ -21,14 +20,14 @@ export default class ConnectionStatus extends Component {
         //check for connection status
         this.connectionStatus();
     }
-
-
-
     connectionStatus(){
+        //TODO: Remove jQuery selector when upgrade to toolbox 2.0
+        let $body = $('body');
         Tracker.autorun(() => {
             let status = Meteor.status().status;
 
             if(status != 'connected') {
+                $body.addClass('disconnected');
                 this.setState({
                     connectionBar: true,
                     barMessage: "Unable to connect to the internet. Please check your network settings."
@@ -36,14 +35,13 @@ export default class ConnectionStatus extends Component {
             }
             else {
                 this.setState({ connectionBar: false });
+                setTimeout(() => {
+                    $body.removeClass('disconnected');
+                }, 3000)
             }
         });
     }
-
-
-
     render() {
-        console.log("connection status");
         return (
             <Snackbar
                 theme={theme}
@@ -51,5 +49,4 @@ export default class ConnectionStatus extends Component {
                 label={this.state.barMessage}
                 />
         )}
-
 }
