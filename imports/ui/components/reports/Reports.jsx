85c07333c47636e856df09fc6406826cb780c579
Syed Asher Ahmed <syedasherahmed@yahoo.com>
2017-06-22 13:46:37 +0500

    Pagination in Reports

--- imports/ui/components/reports/Reports.jsx
+++ imports/ui/components/reports/Reports.jsx
@@ -4,6 +4,7 @@ import { createContainer } from 'meteor/react-meteor-data';
 
 import FilterBar from '/imports/ui/components/filters/FilterBar.jsx';
 import TransactionsTable from '/imports/ui/components/reports/TransactionsTable.jsx';
+import Pagination from '/imports/ui/components/reports/Pagination.jsx';
 import Arrow from '/imports/ui/components/arrow/Arrow.jsx';
 
 import theme from './theme';
@@ -21,6 +22,7 @@ class ReportsPage extends Component {
             <div className={theme.reports}>
                 <FilterBar />
                 <TransactionsTable />
+                <Pagination />
             </div>
         );
     }
