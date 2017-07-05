d5ee98398b0589505c7f560c626727b0dc370af9
zubair khanzada <zubairkhanzada7@gmail.com>
2017-07-05 15:42:18 +0500

    create success and error page

--- imports/ui/components/payments/EasyPayCallBack.jsx
+++ imports/ui/components/payments/EasyPayCallBack.jsx
@@ -26,35 +26,33 @@ class EasyPaisaCallBack extends Component {
         return (
             <div className={theme.easyPaisaBanner}>
                 <Card className={theme.cardContent}>
-                    {/*<div className="">*/}
-                        {/*<img className={theme.easyPaisaImg} src="../assets/images/download.png" alt=""/>*/}
-                        {/*<span className={theme.succesful}>your transaction was succesful</span>*/}
-                        {/*<div className={theme.transactionId}>*/}
-                            {/*<span>Transaction ID</span>*/}
-                            {/*<span>00112233</span>*/}
-                        {/*</div>*/}
-                        {/*<div className={theme.transactionId}>*/}
-                            {/*<span>Amount</span>*/}
-                            {/*<span>5.00</span>*/}
-                        {/*</div>*/}
-                        {/*<div className={theme.transactionId}>*/}
-                            {/*<span>Description</span>*/}
-                            {/*<span>Tool</span>*/}
-                        {/*</div>*/}
-                        {/*<Link to="/app">*/}
-                            {/*<Button className={theme.easyPaisaBtn} label='Go to Home' raised />*/}
-                        {/*</Link>*/}
-
-                    {/*</div>*/}
-
                     <div className="">
                         <img className={theme.easyPaisaImg} src="../assets/images/download.png" alt=""/>
-                        <p className={theme.errorText}>Your transaction could not be processed due to a technical error. please try again</p>
+                        <span className={theme.succesful}>your transaction was succesful</span>
+                        <div className={theme.transactionId}>
+                            <span>Transaction ID</span>
+                            <span>00112233</span>
+                        </div>
+                        <div className={theme.transactionId}>
+                            <span>Amount</span>
+                            <span>5.00</span>
+                        </div>
+                        <div className={theme.transactionId}>
+                            <span>Description</span>
+                            <span>Tool</span>
+                        </div>
                         <Link to="/app">
-                            <Button className={theme.easyPaisaBtn} label='Go Back' raised />
+                            <Button className={theme.easyPaisaBtn} label='Go to Home' raised />
                         </Link>
-
                     </div>
+
+                    {/*<div className="">*/}
+                        {/*<img className={theme.easyPaisaImg} src="../assets/images/download.png" alt=""/>*/}
+                        {/*<p className={theme.errorText}>Your transaction could not be processed due to a technical error. please try again</p>*/}
+                        {/*<Link to="/app">*/}
+                            {/*<Button className={theme.easyPaisaBtn} label='Go Back' raised />*/}
+                        {/*</Link>*/}
+                    {/*</div>*/}
 
                 </Card>
             </div>
