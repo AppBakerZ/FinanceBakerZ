259dead8f9df0f52249c4de5dcede964fbfd86f9
Abdul Hameed <raza2022@gmail.com>
2017-07-20 16:15:55 +0500

    Expense Details page made dynamic

--- imports/ui/components/transactions/childs/viewExpense.jsx
+++ imports/ui/components/transactions/childs/viewExpense.jsx
@@ -1,6 +1,11 @@
 import React, { Component, PropTypes } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import moment from 'moment';
+import { Transactions } from '../../../../api/transactions/transactions.js'
+import { Accounts } from '../../../../api/accounts/accounts'
+import { routeHelpers } from '../../../../helpers/routeHelpers.js'
+import { userCurrencyHelpers } from '../../../../helpers/currencyHelpers'
+
 
 import { Button, Table, FontIcon, Autocomplete, Dropdown, DatePicker, Dialog, Input, ProgressBar, Snackbar, Card } from 'react-toolbox';
 import {FormattedMessage, FormattedNumber, intlShape, injectIntl, defineMessages} from 'react-intl';
@@ -11,23 +16,79 @@ class viewExpense extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            active: false
+        };
 
+    }
+
+    editTransaction(){
+        routeHelpers.changeRoute(`/app/transactions/expense/edit/${this.props.params.id}`)
+    }
+
+    removeTransaction(){
+        const { id } = this.props.params;
+        Meteor.call('expenses.remove', {
+            expense: {
+                _id: id
+            }
+        }, (err, response) => {
+            if(err){
+                this.setState({
+                    active: true,
+                    barMessage: err.reason,
+                    barIcon: 'error_outline',
+                    barType: 'cancel'
+                });
+            }else{
+                routeHelpers.changeRoute('/app/reports', 1200);
+                this.setState({
+                    active: true,
+                    barMessage: 'Expense deleted successfully',
+                    barIcon: 'done',
+                    barType: 'accept'
+                });
+            }
+        });
+    }
+
+    handleBarClick (event, instance) {
+        this.setState({ active: false });
+    }
+
+    handleBarTimeout (event, instance) {
+        this.setState({ active: false });
     }
     /*************** template render ***************/
     render() {
+        let { transaction, account } = this.props;
+        let {_id, transactionAt, category, amount } = transaction;
+        let { bank, number } = account;
+        let date = moment(transactionAt).format('DD-MMM-YYYY');
+        //remove the bank prefix from bank account
+        bank = bank && bank.split("bank-")[1];
         return (
             <div className={theme.viewExpense}>
                 <div className="container">
                     <div className={theme.titleBox}>
+                        <Snackbar
+                            action='Dismiss'
+                            active={this.state.active}
+                            icon={this.state.barIcon}
+                            label={this.state.barMessage}
+                            timeout={2000}
+                            onClick={this.handleBarClick.bind(this)}
+                            onTimeout={this.handleBarTimeout.bind(this)}
+                            type={this.state.barType}
+                        />
                         <h3>bank deposit</h3>
                         <div className={theme.rightButtons}>
-                            <Button
+                            <Button onClick={this.editTransaction.bind(this)}
                                 className='header-buttons'
                                 label="edit"
                                 name='Income'
                                 flat />
-                            <Button
+                            <Button onClick={this.removeTransaction.bind(this)}
                                 className='header-buttons'
                                 label="delete"
                                 name='Expense'
@@ -36,11 +97,11 @@ class viewExpense extends Component {
                     </div>
                     <div className={theme.bankContent}>
                         <div className={theme.depositContent}>
-                            <h6>Transaction ID: <span>009981</span></h6>
-                            <h6>Date: <span>05-December-2016</span></h6>
-                            <h5>Deposited in: <span>Standard Chartered</span></h5>
-                            <h5>Account Number: <span>00971322001</span></h5>
-                            <h5>Amount: <span className={theme.price}>10,000</span> <span>PKR</span></h5>
+                            <h6>Transaction ID: <span>{_id}</span></h6>
+                            <h6>Date: <span>{date}</span></h6>
+                            <h5>Deposited in: <span>{bank}</span></h5>
+                            <h5>Account Number: <span>{number}</span></h5>
+                            <h5>Amount: <i className={userCurrencyHelpers.loggedUserCurrency()}></i> <span className={theme.price}><FormattedNumber value={amount}/></span></h5>
                         </div>
                         <div className={theme.accountContent}>
                             <h5>Sender Name: <span>Saeed Anwar</span></h5>
@@ -48,7 +109,7 @@ class viewExpense extends Component {
                             <h5>Account Number: <span>009123455670</span></h5>
                         </div>
                         <div className={theme.projectContent}>
-                            <h5>Project: <span>Logo Design</span></h5>
+                            <h5>Category: <span>{category ? category.name : 'Not Found'}</span></h5>
                         </div>
                     </div>
                 </div>
@@ -62,9 +123,18 @@ viewExpense.propTypes = {
 
 };
 
-viewExpense = createContainer(() => {
+viewExpense = createContainer((props) => {
+    const { id } = props.params;
+    let accountId;
+    Meteor.subscribe('transactions.single', id);
+    Meteor.subscribe('accounts');
+    Meteor.subscribe('categories');
+    const transaction = Transactions.findOne(id);
+    transaction && (accountId = transaction.account);
+    const account = Accounts.findOne(accountId);
     return {
-
+        transaction: transaction ? transaction : {},
+        account: account ? account : {}
     };
 }, viewExpense);
 
