c39c6eb6c7b44d6fde5993ffdedf1f9d6f19512e
tariq <tariqsidd@ymail.com>
2017-08-10 12:43:58 +0000

    propTypesUpdate

--- imports/ui/components/filters/FilterBy.jsx
+++ imports/ui/components/filters/FilterBy.jsx
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 import { createContainer } from 'meteor/react-meteor-data';
 import { Dropdown } from 'react-toolbox';
 import {intlShape, injectIntl, defineMessages} from 'react-intl';
