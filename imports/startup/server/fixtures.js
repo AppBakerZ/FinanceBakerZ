619162f92c4fec54c79aff2024b8d5bab462655c
Abdul Hameed <raza2022@gmail.com>
2017-09-08 17:37:21 +0500

    initial fallback code added for old users

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -1,6 +1,8 @@
 // fill the DB with example data on startup
 
 import { Meteor } from 'meteor/meteor';
+import { Categories } from '/imports/api/categories/categories.js'
+import { Projects } from '/imports/api/projects/projects.js'
 
 
 
@@ -20,5 +22,18 @@ function setGravatars() {
 
 // if the database is empty on server start, create some sample data.
 Meteor.startup(() => {
+    //TODO: remove below code after one time deployment
+    let availableUsers = Meteor.users.find().fetch();
+    if(availableUsers && availableUsers.length){
+        availableUsers.forEach((user) => {
+            if(!Categories.findOne({owner: user._id})){
+                Categories.insert({owner: user._id, name: 'Default Category', icon: 'icon-icons_building' });
+            }
+            if(!Projects.findOne({owner: user._id})){
+                Projects.insert({owner: user._id, name: 'Default Project', type: 'fixed', status: 'progress', startAt: new Date()});
+            }
+        })
+    }
+
     setGravatars();
 });
