7c96355d74633df6527f74132f3550a0e5e621fa
Abdul Hameed <raza2022@gmail.com>
2017-10-19 18:40:42 +0500

    forgot password functionality implemented

--- imports/startup/server/email.js
+++ imports/startup/server/email.js
@@ -1,6 +1,10 @@
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base'
 
 Meteor.startup(() => {
-    //send email with mailgun
     process.env.MAIL_URL = `smtp://${encodeURIComponent(Meteor.settings.SESUser)}:${encodeURIComponent(Meteor.settings.SESPASSWORD)}@email-smtp.us-east-1.amazonaws.com:465`;
+    Accounts.emailTemplates.from = "Finance Bakerz <no-reply@financebakerz.com>"
+    Accounts.urls.resetPassword = function(token) {
+        return Meteor.absoluteUrl('forgotPassword/' + token);
+    };
 });
--- imports/ui/components/auth/ForgotPassword.jsx
+++ imports/ui/components/auth/ForgotPassword.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Accounts } from 'meteor/accounts-base'
+import { routeHelpers } from '/imports/helpers/routeHelpers'
 
-import { Input, Button } from 'react-toolbox';
+import { Input, Button, ProgressBar, Snackbar} from 'react-toolbox';
 import { Link } from 'react-router'
 
 import theme from './theme';
@@ -36,25 +38,32 @@ class ForgotPassword extends Component {
         super(props);
 
         this.state = {
-            isPassword: false,
-            loading: false
+            email: '',
+            newPassword: '',
+            confirmPassword: '',
+            isToken: props.params.token || false,
+            loading: false,
+            active: false,
         }
     }
 
     showEmail(){
         const { formatMessage } = this.props.intl;
         return(
-            <form  onSubmit={this.onSubmit.bind(this)}  className="login" autoComplete={'off'}>
+            <form  onSubmit={this.onSubmit.bind(this, 'forgotPassword')}  className="login" autoComplete={'off'}>
                 <div className={theme.logoWithText}>
                     <img src={'../assets/images/logo-withText.png'} alt="Logo-with-text" />
                 </div>
-                <Input type='text' label={formatMessage(il8n.EMAIL)}
-                       name='EnterEmail'
+                <Input type='email' label={formatMessage(il8n.EMAIL)}
+                       default
+                       name='email'
+                       value={this.state.email}
+                       onChange={this.onChange.bind(this)}
                        maxLength={ 30 }
                        required
                     />
                 <div className={theme.forgotBtn}>
-                    <Button type='submit' disabled={this.props.loading}
+                    <Button type='submit' disabled={this.state.loading}
                             label={formatMessage(il8n.SUBMIT_BUTTON)} raised primary />
                     <Link to={`/`}>
                         <Button type='button'
@@ -68,48 +77,135 @@ class ForgotPassword extends Component {
     showPassword(){
         const { formatMessage } = this.props.intl;
         return(
-            <form  onSubmit={this.onSubmit.bind(this)}  className="login" autoComplete={'off'}>
+            <form onSubmit={this.onSubmit.bind(this, 'resetPassword')}  className="login" autoComplete={'off'}>
                 <div className={theme.logoWithText}>
                     <img src={'../assets/images/logo-withText.png'} alt="Logo-with-text" />
                 </div>
                 <Input type='password' label={formatMessage(il8n.ENTER_PASSWORD)}
-                       name='EnterNewPassword'
+                       name='newPassword'
+                       value={this.state.newPassword}
+                       onChange={this.onChange.bind(this)}
                        maxLength={ 30 }
                        required
                     />
                 <Input type='password' label={formatMessage(il8n.RE_ENTER_PASSWORD)}
-                       name='Re-Enter New Password'
+                       name='confirmPassword'
+                       value={this.state.confirmPassword}
+                       onChange={this.onChange.bind(this)}
                        maxLength={ 30 }
                        required
                     />
                 <div className={theme.forgotBtn}>
-                    <Button type='button'
+                    <Button type='submit' disabled={this.state.loading}
                             label={formatMessage(il8n.SAVE_BUTTON)} raised primary />
                 </div>
             </form>
         )
     }
 
-    onSubmit(event){
-        event.preventDefault();
-        let obj = {
-            email: {
-                from:'no-reply@financeBakerz.com',
-                to: 'raza2022@gmail.com'
+    forgotPassword(){
+        let { email } = this.state;
+
+        Accounts.forgotPassword({email}, (err, response) => {
+            if(!err){
+                this.setState({
+                    active: true,
+                    barMessage: 'A confirmation email sent to your email address.',
+                    barIcon: 'done',
+                    barType: 'accept'
+                });
+            }else{
+                this.setState({
+                    disableButton: false,
+                    active: true,
+                    barMessage: err.reason,
+                    barIcon: 'error_outline',
+                    barType: 'cancel'
+                });
             }
-        };
-        Meteor.call('emails.send', obj, (err, res) => {
-            console.log('err', err)
-            console.log('res', res)
+            this.setState({
+                loading: false,
+                email: ''
+            });
         });
-        // this.setState({isPassword: true});
+    }
+
+    resetPassword(){
+        let { newPassword, confirmPassword, isToken} = this.state;
+        if(newPassword !== confirmPassword){
+            this.setState({
+                disableButton: false,
+                active: true,
+                loading: false,
+                barMessage: 'Passwords do not match',
+                barIcon: 'error_outline',
+                barType: 'cancel'
+            });
+            return false;
+        }
+        Accounts.resetPassword(isToken, newPassword, (err, response) => {
+            console.log(err)
+            console.log(response)
+            if(!err){
+                routeHelpers.changeRoute('/login', 1200);
+                this.setState({
+                    active: true,
+                    barMessage: 'Password updated successfully',
+                    barIcon: 'done',
+                    barType: 'accept'
+                });
+            }else{
+                this.setState({
+                    disableButton: false,
+                    active: true,
+                    barMessage: err.reason,
+                    barIcon: 'error_outline',
+                    barType: 'cancel'
+                });
+            }
+            this.setState({
+                loading: false,
+                newPassword: '',
+                confirmPassword: ''
+            });
+        });
+    }
+
+    onSubmit(type, event){
+        this.setState({
+            loading: true
+        });
+        event.preventDefault();
+        type === 'forgotPassword' ? this.forgotPassword() : this.resetPassword();
+    }
+
+    handleBarClick (event, instance) {
+        this.setState({ active: false });
+    }
+
+    handleBarTimeout (event, instance) {
+        this.setState({ active: false });
+    }
+
+    onChange (val, e) {
+        this.setState({[e.target.name]: val});
     }
 
     render()
     {
         return (
             <div>
-                {this.state.isPassword === false ? this.showEmail() : this.showPassword()}
+                <Snackbar
+                    action='Dismiss'
+                    active={this.state.active}
+                    icon={this.state.barIcon}
+                    label={this.state.barMessage}
+                    timeout={2000}
+                    onClick={this.handleBarClick.bind(this)}
+                    onTimeout={this.handleBarTimeout.bind(this)}
+                    type={this.state.barType}
+                />
+                {this.state.isToken === false ? this.showEmail() : this.showPassword()}
             </div>
         )
     }
