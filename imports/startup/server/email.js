8b4562e14247b39b09ef0fd84090d857e94be2cc
Abdul Hameed <raza2022@gmail.com>
2017-10-21 15:38:30 +0500

    add dynamic template within layout

--- imports/startup/server/email.js
+++ imports/startup/server/email.js
@@ -7,4 +7,38 @@ Meteor.startup(() => {
     Accounts.urls.resetPassword = function(token) {
         return Meteor.absoluteUrl('forgotPassword/' + token);
     };
+    Accounts.emailTemplates.resetPassword = {
+
+        subject() {
+            return "Reset Password Request!";
+        },
+        html(user, url) {
+            SSR.compileTemplate('emailLayout', Assets.getText('emailLayout.html'));
+            SSR.compileTemplate('test', Assets.getText('test.html'));
+            Template.emailLayout.helpers({
+                getDocType: function() {
+                    return "<!DOCTYPE html>";
+                }
+            });
+            let css = Assets.getText('bootstrap.min.css');
+            let customStyle = Assets.getText('forgotPassword.css');
+            let Group = Assets.getText('Group.png');
+            let footerLogo = Assets.getText('footer-logo.png');
+            let data = {
+                css,
+                customStyle,
+            };
+            return SSR.render('emailLayout', {
+                css: css,
+                customStyle,
+                template: "test",
+                Group,
+                footerLogo,
+                data: {
+                    Group,
+                    footerLogo
+                },
+            });
+        }
+    }
 });
