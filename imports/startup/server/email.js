7eaf59fb0677043fb625bb8e581aeb65e0f7ab12
Abdul Hameed <raza2022@gmail.com>
2017-10-23 17:14:25 +0500

    new template added after compile

--- imports/startup/server/email.js
+++ imports/startup/server/email.js
@@ -3,7 +3,7 @@ import { Accounts } from 'meteor/accounts-base'
 
 Meteor.startup(() => {
     process.env.MAIL_URL = `smtp://${encodeURIComponent(Meteor.settings.SESUser)}:${encodeURIComponent(Meteor.settings.SESPASSWORD)}@email-smtp.us-east-1.amazonaws.com:465`;
-    Accounts.emailTemplates.from = "Finance Bakerz <no-reply@financebakerz.com>"
+    Accounts.emailTemplates.from = "Finance Bakerz <no-reply@financebakerz.com>";
     Accounts.urls.resetPassword = function(token) {
         return Meteor.absoluteUrl('forgotPassword/' + token);
     };
@@ -13,23 +13,14 @@ Meteor.startup(() => {
             return "Reset Password Request!";
         },
         html(user, url) {
-            SSR.compileTemplate('compiled', Assets.getText('compiled.html'));
-            // let html = Assets.getText('compiled.html')
-            // SSR.compileTemplate('compiled', Assets.getText('compiled.html'));
-            // Template.emailLayout.helpers({
-            //     getDocType: function() {
-            //         return "<!DOCTYPE html>";
-            //     }
-            // });
-            //
-            // let htmlString = SSR.render('emailLayout', {
-            //     template: "compiled",
-            //     data: {
-            //         url
-            //     },
-            // });
-            // console.log(html);
-            return SSR.render( 'compiled', {url} )
+            //get precompiled template which compiled with http://premailer.dialect.ca/
+            SSR.compileTemplate('t', Assets.getText('t.html'));
+            let data = {
+                url,
+                userName: user.profile.fullName
+            };
+            //then render the compiled template with data
+            return SSR.render('t', data )
         }
     }
 });
