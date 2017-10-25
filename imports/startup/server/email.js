cc3daf011b206171fb6faf86ce58a5e0ab647358
Abdul Hameed <raza2022@gmail.com>
2017-10-25 13:57:03 +0500

    new template for forgot password

--- imports/startup/server/email.js
+++ imports/startup/server/email.js
@@ -3,7 +3,7 @@ import { Accounts } from 'meteor/accounts-base'
 
 Meteor.startup(() => {
     process.env.MAIL_URL = `smtp://${encodeURIComponent(Meteor.settings.SESUser)}:${encodeURIComponent(Meteor.settings.SESPASSWORD)}@email-smtp.us-east-1.amazonaws.com:465`;
-    Accounts.emailTemplates.from = "Finance Bakerz <no-reply@financebakerz.com>";
+    Accounts.emailTemplates.from = "FinanceBakerz <no-reply@financebakerz.com>";
     Accounts.urls.resetPassword = function(token) {
         return Meteor.absoluteUrl('forgotPassword/' + token);
     };
